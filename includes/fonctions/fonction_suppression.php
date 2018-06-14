f303b1e34265f919db0b78a6b96df8d1dfa15567
Remousses <remy91490@hotmail.fr>
2018-06-14 19:54:00 +0200

    fonction_supprimer accessible que pour comite

--- includes/fonctions/fonction_suppression.php
+++ includes/fonctions/fonction_suppression.php
@@ -2,8 +2,20 @@
     require_once 'connexionDB.php';
 
     function archiverAlerte($idAlerte){
-        $archiver = DBConnexion()->prepare('UPDATE alerte SET Statut = 2 WHERE Id_alerte = ' . $idAlerte);
-        $archiver->execute();
-        $archiver->closeCursor();
+        $connexion = DBConnexion();
+        $archiverAlerte = $connexion->prepare('UPDATE alerte SET Statut = 2 WHERE Id_alerte = ' .$idAlerte);
+        $rechercheProjet = $connexion->prepare('SELECT Nom_projet FROM projet WHERE Id_alerte = ' . $idAlerte);
+
+        if($rechercheProjet->execute() && $rechercheProjet->rowCount() > 0){
+            $archiverProjet = $connexion->prepare('UPDATE projet SET Statut = 2 WHERE Id_alerte = ' . $idAlerte);
+            $archiverProjet->execute();
+            $archiverProjet->closeCursor();
+        }
+        
+        $archiverAlerte->execute();
+
+        $rechercheProjet->closeCursor();
+        $archiverAlerte->closeCursor();
+            
     }
 ?>
